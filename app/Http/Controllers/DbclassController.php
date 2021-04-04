cde5cdf9b3497d5be120427bc7c6a01cc61a21fd
runamumu <runaruna.69.muse@gmail.com>
2021-04-04 13:54:26 +0900

    0404

---
+++ app/Http/Controllers/DbclassController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+
+class DbclassController extends Controller
+{
+    public function index(Request $request)
+    {
+        $items = DB::select('serect * from people');
+        return view ('db', ['items' => $items]);
+    }
+}
---
+++ resources/views/db.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <style>
+    th{
+      background-color: black;
+      color:white;
+      padding: 5px 30px;
+    }
+
+    td{
+      border: 1px solid black;
+      padding: 5px 30px;
+      text-align: center;
+    }
+  </style>
+</head>
+<body>
+  <table>
+    <tr>
+      <th>name</th>
+      <th>age</th>
+    </tr>
+    @foreach ($items as $item)
+    <tr>
+      <td>
+        {{$item->name}} 
+      </td>
+    </tr>       
+    @endforeach
+  </table> 
+</body>
+</html>
