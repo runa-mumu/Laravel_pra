0f4dc74c9828b721ada89c80bd988f402658b528
runamumu <runaruna.69.muse@gmail.com>
2021-04-05 16:12:23 +0900

    0405

--- app/Http/Controllers/DbclassController.php
+++ app/Http/Controllers/DbclassController.php
@@ -10,7 +10,24 @@ class DbclassController extends Controller
 {
     public function index(Request $request)
     {
-        $items = DB::select('serect * from people');
+        $items = DB::select('select * from people');
         return view ('db', ['items' => $items]);
     }
+    public function add(Request $request)
+    {
+        return view('add');
+    }
+    public function create(Request $request)
+    {
+        $param = [
+            'id' => $request->id,
+            'name' => $request->name,
+            'age' => $request->age,
+        ];
+        DB::insert('insert into people(id, name, age) values (:id, :name, :age)', $param );
+        return redirect('/');
+    }
+
 }
+
+
--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DbclassController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', [DbclassController::class,'index']);
+Route::get('/', [DbclassController::class, 'index']);
+Route::get('/add', [DbclassController::class, 'add']);
+Route::post('/add', [DbclassController::class, 'create']);
