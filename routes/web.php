3b44e477f6aa1d535582e3b3541a418f61380acf
runamumu <runaruna.69.muse@gmail.com>
2021-04-06 13:34:23 +0900

    0406

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,5 @@ use App\Http\Controllers\DbclassController;
 Route::get('/', [DbclassController::class, 'index']);
 Route::get('/add', [DbclassController::class, 'add']);
 Route::post('/add', [DbclassController::class, 'create']);
+Route::get('/edit', [DbclassController::class, 'edit']);
+Route::post('/edit', [DbclassController::class, 'update']);
